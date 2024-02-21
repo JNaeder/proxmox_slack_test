2f3cb614520226e93c4b05fbcca5b428d60887aa
John Naeder <johnnaeder@Johns-MacBook-Pro.local>
2024-02-21 16:37:45 -0500

    Added endpoint for saying hi to Kevan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from slack_bolt import App
 from slack_bolt.adapter.fastapi import SlackRequestHandler
 from dotenv import load_dotenv
@@ -25,6 +26,26 @@ async def slack_events(request: Request):
     return await handler.handle(request)
 
 
+@api.get("/hi-kevan")
+async def hi_kevan():
+    html_content = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Proxmox Slack Test</title>
+    </head>
+    <body>
+        <h1>Yooo Kevan!</h1>
+        <h2>Check this proxmox business out!</h2>
+        <h3><a href="https://github.com/JNaeder/proxmox_slack_test" 
+        target="_blank">GitHub Repo</a></h3>
+        <h3>Super simple slack app for testing shit</h3>
+    </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+
 @app.message()
 def say_hello(message, say):
     say(f"Hey there <@{message['user']}>!")
@@ -32,4 +53,5 @@ def say_hello(message, say):
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(api, host="0.0.0.0", port=8000)
